ff619b77790a30140bd7e74aa27a890cd95f6eb7
Bertybb <bertrand.tb@gmail.com>
2016-10-22 14:22:55 -0400

    Première page fonctionnelle

---
+++ app/src/main/java/com/ets/gti710/deliveryapp/InitialPage.java
@@ -0,0 +1,13 @@
+package com.ets.gti710.deliveryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitialPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_page);
+    }
+}
